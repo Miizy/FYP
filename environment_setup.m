7fb6f9dba20f2436fe621bb6feed0682b11cebf5
Miizy <miizyqq@gmail.com>
2024-06-13 14:43:27 +0800

    Changed setup to a function instead of a script

--- environment_setup.m
+++ environment_setup.m
@@ -1,22 +1,24 @@
-% Define problem space
-x_max = 10;
-y_max = 10;
-start = [0, 0];
-goal = [9, 9];
-step_size = 0.5;
-max_iter = 1000;
+function [start, goal, x_max, y_max, step_size, max_iter, obstacles] = environment_setup()
+    % Define problem space
+    x_max = 10;
+    y_max = 10;
+    start = [0, 0];
+    goal = [9, 9];
+    step_size = 0.5;
+    max_iter = 1000;
 
-% Define obstacles (example: a 1x1 square centered at (5,5))
-obstacles = [4.5, 4.5, 1, 1]; % [x, y, width, height]
+    % Define obstacles (example: a 1x1 square centered at (5,5))
+    obstacles = [4.5, 4.5, 1, 1]; % [x, y, width, height]
 
-% Create figure
-figure;
-hold on;
-axis([0 x_max 0 y_max]);
-plot(start(1), start(2), 'go', 'MarkerSize', 10, 'MarkerFaceColor', 'g');
-plot(goal(1), goal(2), 'ro', 'MarkerSize', 10, 'MarkerFaceColor', 'r');
+    % Create figure
+    figure;
+    hold on;
+    axis([0 x_max 0 y_max]);
+    plot(start(1), start(2), 'go', 'MarkerSize', 10, 'MarkerFaceColor', 'g');
+    plot(goal(1), goal(2), 'ro', 'MarkerSize', 10, 'MarkerFaceColor', 'r');
 
-% Plot obstacles
-for i = 1:size(obstacles, 1)
-    rectangle('Position', obstacles(i,:), 'FaceColor', [0.5 0.5 0.5]);
+    % Plot obstacles
+    for i = 1:size(obstacles, 1)
+        rectangle('Position', obstacles(i,:), 'FaceColor', [0.5 0.5 0.5]);
+    end
 end
