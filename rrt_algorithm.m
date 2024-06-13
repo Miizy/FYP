cc8d1e1d7b061a7dacd6ffc5308589a71125e5b0
Miizy <miizyqq@gmail.com>
2024-06-13 14:45:29 +0800

    Collision checking function moved to utility class.

--- rrt_algorithm.m
+++ rrt_algorithm.m
@@ -15,7 +15,7 @@ function [tree, path] = rrt_algorithm(start, goal, x_max, y_max, step_size, max_
         new_point = steer(nearest_node, rand_point, step_size);
         
         % Check if new point is in free space (collision-free)
-        if ~isCollision(nearest_node, new_point, x_max, y_max, obstacles, step_size)
+        if ~PathingUtility.isCollision(nearest_node, new_point, x_max, y_max, obstacles, step_size)
             % Add new point to the tree
             tree = [tree; new_point];
             parents = [parents; nearest_idx];
@@ -58,34 +58,3 @@ function new_point = steer(from, to, step_size)
     new_point = from + min(step_size, distance) * direction;
 end
 
-function collision = isCollision(from, to, x_max, y_max, obstacles, step_size)
-    % Check if the from and to points are within the boundaries of the defined problem space
-    if from(1) < 0 || from(1) > x_max || from(2) < 0 || from(2) > y_max || ...
-       to(1) < 0 || to(1) > x_max || to(2) < 0 || to(2) > y_max
-        collision = true;
-        return;
-    end
-
-    % Number of steps to check along the path from 'from' to 'to'
-    num_steps = ceil(norm(to - from) / step_size);
-
-    % Generate points along the line from 'from' to 'to'
-    for i = 0:num_steps
-        point = from + (i / num_steps) * (to - from);
-
-        % Check if the point is inside any of the obstacles
-        for j = 1:size(obstacles, 1)
-            obs_x = obstacles(j, 1);
-            obs_y = obstacles(j, 2);
-            obs_w = obstacles(j, 3);
-            obs_h = obstacles(j, 4);
-            if point(1) >= obs_x && point(1) <= obs_x + obs_w && ...
-               point(2) >= obs_y && point(2) <= obs_y + obs_h
-                collision = true;
-                return;
-            end
-        end
-    end
-
-    collision = false;
-end
