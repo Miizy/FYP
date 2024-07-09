5136a53ddf0b9fcb2bd07793cc2f0fd694d9e591
Miizy <miizyqq@gmail.com>
2024-07-09 14:10:20 +0800

    sample for using Tester

--- main.m
+++ main.m
@@ -1,14 +1,23 @@
 clc;
 clear;
 close all;
-
+    
 % Load environment
 [start, goal, x_max, y_max, step_size, max_iter, obstacles] = environment_setup();
 
+mytester = Tester();
+mytester = mytester.start_timer();
+
 % Run RRT algorithm
-[tree, path] = rrt_star_algorithm(start, goal, x_max, y_max, step_size, max_iter, obstacles, step_size);
+[tree, path] = improved_rrt_algorithm(start, goal, x_max, y_max, step_size, max_iter, obstacles);
+
+time_taken = mytester.stop_timer();
+disp(['Time taken: ', num2str(time_taken)]);
+mytester.Tree = tree;
+mytester.Path = path;
+node_count = mytester.number_of_nodes();
+disp(['Number of nodes in tree: ', num2str(node_count)]);
+path_length = mytester.path_length();
+disp(['Total path length: ', num2str(path_length)]);
 
 PathingUtility.plot_final_path(path);
-
-length = PathingUtility.calculate_path_length(path);
-disp(['Total path length: ', num2str(length)]);
